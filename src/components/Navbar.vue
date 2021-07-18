e2f3f886d433252b8fe3621e71c4bbcd78d565e1
code-tieumomo <code.tieumomo@gmail.com>
2021-07-18 19:41:41 +0700

    Update web title

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <router-link class="navbar-brand" to="/">I Learn Vuejs</router-link>
+      <router-link class="navbar-brand" to="/">I Learn Vuejs 3 😂</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
